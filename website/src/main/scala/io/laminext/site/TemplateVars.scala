862b8fc314842b9bd9af178773128ccd791cfc80
Iurii Malchenko <i@yurique.com>
2021-02-26 20:55:20 +0200

    setting version to 0.12.0

--- website/src/main/scala/io/laminext/site/TemplateVars.scala
+++ website/src/main/scala/io/laminext/site/TemplateVars.scala
@@ -3,7 +3,7 @@ package io.laminext.site
 object TemplateVars {
 
   val vars = Seq(
-    "laminextVersion" -> "0.12.0-RC1"
+    "laminextVersion" -> "0.12.0"
   )
 
   def apply(s: String): String =
