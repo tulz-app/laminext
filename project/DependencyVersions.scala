8527b7add3c2ccb6ff163dab370dc0bc820363fa
Iurii Malchenko <i@yurique.com>
2021-04-20 09:40:12 +0300

    enable scala 3-RC2 cross building, disable websocket-zio-json module (until it's available for scala 3)

--- project/DependencyVersions.scala
+++ project/DependencyVersions.scala
@@ -4,17 +4,14 @@ import sbt.librarymanagement.CrossVersion
 
 object DependencyVersions {
 
-  val jsdom                       = "16.4.0"
-  val laminar                     = "0.13.0-M1"
-  val `scala-test`                = "3.2.7"
-  val stringdiff                  = "0.3.2"
-  val domtestutils                = "0.14.6"
-  val cats                        = "2.5.0"
-  val `cats-laws`                 = "2.5.0"
-  val `discipline-munit`          = "1.0.6"
-  val `scalacheck-shapeless_1.14` = "1.2.5"
-  val zioJson                     = "0.1.4"
-  val upickle                     = "1.3.11"
+  val jsdom        = "16.4.0"
+  val laminar      = "0.13.0-M1"
+  val `scala-test` = "3.2.7"
+  val stringdiff   = "0.3.2"
+  val domtestutils = "0.14.6"
+  val cats         = "2.6.0"
+  val zioJson      = "0.1.4"
+  val upickle      = "1.3.11"
 
   val circe: Def.Initialize[String] = Def.setting {
     CrossVersion.partialVersion(scalaVersion.value) match {
