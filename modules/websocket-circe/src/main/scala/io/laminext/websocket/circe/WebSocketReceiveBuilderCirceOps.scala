af5ccddd8633efb8d493a16815667b3782ebfb18
Pierre Ricadat <ghostdogpr@gmail.com>
2021-05-23 10:53:28 +0900

    Expose ws protocol

--- modules/websocket-circe/src/main/scala/io/laminext/websocket/circe/WebSocketReceiveBuilderCirceOps.scala
+++ modules/websocket-circe/src/main/scala/io/laminext/websocket/circe/WebSocketReceiveBuilderCirceOps.scala
@@ -9,6 +9,7 @@ class WebSocketReceiveBuilderCirceOps(b: WebSocketReceiveBuilder) {
   @inline def json[Receive, Send](implicit receiveDecoder: Decoder[Receive], sendEncoder: Encoder[Send]): WebSocketBuilder[Receive, Send] =
     new WebSocketBuilder[Receive, Send](
       url = b.url,
+      protocol = b.protocol,
       initializer = initialize.text,
       sender = send.text[Send](sendEncoder(_).noSpaces),
       receiver = receive.text[Receive](decode[Receive])
