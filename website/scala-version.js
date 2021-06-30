0525e441e57a85bf9ef4e264b4ec3f8118848b75
Iurii Malchenko <i@yurique.com>
2021-07-01 01:18:16 +0300

    don't autodetect scala version for website

--- website/scala-version.js
+++ website/scala-version.js
@@ -1,7 +1,12 @@
 const fs = require('fs')
 const build = fs.readFileSync('../build.sbt', 'utf8')
-const scala3 = /^\s*scalaVersion\s*:=\s*ScalaVersions.v3,\s*$/m
-const scalaVersion = scala3.test(build) ? '3.0.0' : '2.13';
+const isScala3 = /^\s*scalaVersion\s*:=\s*ScalaVersions.v3,\s*$/m
+
+const scala2 = '2.13'
+const scala3 = '3.0.0'
+
+// const scalaVersion = isScala3.test(build) ? scala3 : scala2;
+const scalaVersion = scala3
 
 console.log('detected scala version', scalaVersion)
 module.exports = scalaVersion
