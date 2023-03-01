ed7a6a6a8a1090d9eebf26e257d3cdd0200492a6
Iurii Malchenko <i@yurique.com>
2023-03-02 00:55:17 +0100

    frontroute m11

--- project/DependencyVersions.scala
+++ project/DependencyVersions.scala
@@ -12,7 +12,7 @@ object DependencyVersions {
   // -- website
 
   val sourcecode                    = "0.2.7"
-  val frontroute                    = "0.17.0-M10+4-e7b24da0+20230302-0016-SNAPSHOT"
+  val frontroute                    = "0.17.0-M11"
   val `embedded-files-macro`        = "0.2.4"
   val `scala-java-time`             = "2.3.0"
   val `scala-js-macrotask-executor` = "1.0.0"
