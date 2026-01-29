dda0ce46aca06e43cf92c54c274c0ea58d355ea1
Scala Steward <scala_steward@virtuslab.com>
2026-01-29 14:03:21 +0000

    Update domtestutils to 19.0.0

--- project/DependencyVersions.scala
+++ project/DependencyVersions.scala
@@ -4,7 +4,7 @@ object DependencyVersions {
   val laminar      = "16.0.0"
   val `scala-test` = "3.2.9"
   val stringdiff   = "0.3.4"
-  val domtestutils = "17.0.0"
+  val domtestutils = "19.0.0"
   val cats         = "2.10.0"
   val upickle      = "3.1.3"
   val circe        = "0.14.6"
